1b6e9a0dbf6f7202c37afd52d443b65ebe25d475
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-02-26 00:53:28 +0800

    psuh

--- src/components/navbarComponent/ResumeButton.tsx
+++ src/components/navbarComponent/ResumeButton.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Resume from "../../assets/resume.pdf"
 
 const ResumeButton: React.FC = () => {
-  return <button className="bg-teal-800 text-white px-6 py-2 rounded-lg hover:bg-teal-700 transition-colors shadow-md">Resume</button>;
+  const handleDownload = () => {
+    const pdfUrl = Resume;
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Mikko_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <button
+      onClick={handleDownload}
+      className="bg-teal-800 text-white px-6 py-2 rounded-lg hover:bg-teal-700 transition-colors shadow-md cursor-pointer"
+    >
+      Resume
+    </button>
+  );
 };
 
 export default ResumeButton;
