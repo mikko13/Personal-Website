765bb73da2fa211d679c50be842c8129259c91d2
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-04-20 03:23:09 +0800

    fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Code, Terminal } from "lucide-react";
 import "./Loader.css";
 
--- src/components/navbarComponent/NavLogo.tsx
+++ src/components/navbarComponent/NavLogo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Code, Terminal } from "lucide-react";
 
 const NavLogo = () => {
