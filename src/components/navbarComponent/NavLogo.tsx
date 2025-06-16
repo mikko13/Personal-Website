0dd167d8cef88a0a902ad1e82a0454fbad3e4184
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-06-16 17:02:04 +0800

    updated logo

--- src/components/navbarComponent/NavLogo.tsx
+++ src/components/navbarComponent/NavLogo.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/logo2.png"
 
 const NavLogo = () => {
   const [hovered, setHovered] = useState(false);
